446e0f25c29b27a1ea6aa23ede101622b7f5d48d
Renan T. Carneiro <renantc@uspgamedev.org>
2014-06-26 18:14:06 -0300

    Rectangles. Now figure out how to clean up code using it.

--- proto2/rect.lua
+++ proto2/rect.lua
@@ -9,9 +9,28 @@ rect = lux.object.new{
 }
 
 function rect:iscolliding(other)
+  if not (other.x or other.y or other.height or other.width) then
+    return false
+  end
+  if self.x + self.width >= other.x and other.x + other.width >= self.x then
+    if self.y <= other.y + other.height and self.y + self.height >= other.y then
+      return true
+    end
+  end
+  return false
+end
 
+function rect:shrinkh(n)
+  self.x = self.x + n
+  self.width = self.width - 2 * n
+end
+
+function rect:shrinkv(n)
+  self.y = self.y + n
+  self.height = self.height - 2 * n
 end
 
 function rect:shrink(n)
-
+  self:shrinkh(n)
+  self:shrinkv(n)
 end
